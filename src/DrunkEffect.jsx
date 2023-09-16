9e45a1058f552e7275a02c9cc249b9a697bf143a
uen <77583577+uenvermelih@users.noreply.github.com>
2023-09-16 23:56:32 +0200

    first

---
+++ src/DrunkEffect.jsx
@@ -0,0 +1,39 @@
+import { Effect, BlendFunction } from "postprocessing";
+
+const fragmentShader = /* glsl */`
+    uniform float frequency;
+    uniform float amplitude;
+    uniform float offset;
+
+    void mainUv(inout vec2 uv) 
+    {
+        uv.y += sin(uv.x * frequency + offset) * amplitude;
+    }
+
+    void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor)
+    {
+        outputColor = vec4(0.8, 1.0, 0.5, inputColor.a);
+    }
+`
+
+export default class DrunkEffect extends Effect {
+
+    constructor( { frequency, amplitude, blendFunction= BlendFunction.DARKEN } ) {
+        super(
+            "DrunkEffect",
+            fragmentShader,
+            {
+                blendFunction: blendFunction,
+                uniforms: new Map([
+                    [ "frequency", { value: frequency } ],
+                    [ "amplitude", { value: amplitude } ],
+                    [ "offset", { value: 0 } ],
+                ])
+            }
+        )
+    }
+
+    update(renderer, inputBuffer, deltaTime) {
+        this.uniforms.get("offset").value += deltaTime
+    }
+}
